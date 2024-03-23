eeb980210f3dd1456193c5a804d36f1d798e5e9e
RamyAhmedI <91535329+RamyAhmedI@users.noreply.github.com>
2024-03-23 14:40:41 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -24,5 +24,34 @@ name = 'Test 1'
 url = 'http://localhost:5000/’'
 result = checkServiceForWord(url, 'URl')
 saveResult(name, url, result)
+print(result)
+
+# Test 2
+name = 'Test 2'
+url = 'http://localhost:5000/getProducts’'
+result = checkServiceForWord(url, 'banana')
+saveResult(name, url, result)
+print(result)
+
+# Test 3
+name = 'Test 3'
+url = 'http://127.0.0.1:5000/insertProducts?api_key=login’'
+result = checkServiceForWord(url, 'inserted')
+saveResult(name, url, result)
+print(result)
+
+# Test 4
+name = 'Test 4'
+url = 'http://127.0.0.1:5000/insertProducts’'
+result = checkServiceForWord(url, 'Invalid')
+saveResult(name, url, result)
+print(result)
+
+# Test 5
+name = 'Test 5'
+url = 'http://127.0.0.1:5000/getTitles’'
+result = checkServiceForWord(url, 'banana')
+saveResult(name, url, result)
+print(result)
 # finish up
 f.close()
